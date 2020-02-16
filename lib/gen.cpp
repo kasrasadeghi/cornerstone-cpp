8170bbd6234a3e36c59e235d791fa4173f6aaf22
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-02-16 15:06:49 -0600

    slightly improve argument type mismatch

--- lib/gen.cpp
+++ lib/gen.cpp
@@ -474,7 +474,7 @@ struct LLVMGenerator {
     {
       print("(");
       int i = 0;
-      CHECK(texp.size() == types.size(), "wrong argument count for " + texp.paren());
+      CHECK(texp.size() == types.size(), "wrong argument count for " + texp.paren() + ", expecting '" + types.paren() + "'");
       for (Texp arg : texp)
         {
           Type(types[i], types_proof[i]);
