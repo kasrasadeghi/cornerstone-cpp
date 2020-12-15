299ddadb5b66f6ceca55f8a1e35e80d07b37970a
kasrasadeghi <kasra.sadeghi1@gmail.com>
2020-12-14 20:30:52 -0500

    fix whitespace in grammar.cpp

--- lib/grammar.cpp
+++ lib/grammar.cpp
@@ -45,17 +45,17 @@ std::optional<std::string_view> Grammar::getKeyword(std::string_view s) const
 
 void UnionMatch(const Grammar& g,
                 std::string_view parent_type_name,
-                const Texp& texp, 
+                const Texp& texp,
                 const Texp& proof,
-                std::vector<std::pair<std::string_view, std::function<void(const Texp&, const Texp&)>>> cases, 
+                std::vector<std::pair<std::string_view, std::function<void(const Texp&, const Texp&)>>> cases,
                 bool exhaustive)
   {
     CHECK(g.parseType(parent_type_name), "parent choice '" + std::string(parent_type_name) + "' not in grammar")
     Grammar::Type texp_type = parseChoice(g, proof, parent_type_name);
-    for (auto pair : cases) 
+    for (auto pair : cases)
       {
         auto case_name = pair.first;
-        auto case_f = pair.second;  
+        auto case_f = pair.second;
         Grammar::Type case_type = CHECK_UNWRAP(g.parseType(case_name), "case '" + std::string(case_name) + "' not in grammar.");
         if (case_type == texp_type)
           {
