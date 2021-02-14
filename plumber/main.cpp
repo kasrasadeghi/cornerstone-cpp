e6901772223a39f4a52d19ce5f509236db9ef56d
kasrasadeghi <kasra.sadeghi1@gmail.com>
2021-02-13 23:37:29 -0500

    plumber: initial working version

--- plumber/main.cpp
+++ plumber/main.cpp
@@ -1,14 +1,20 @@
+// cornerstone includes
 #include "parser.hpp"
-#include "pass.hpp"
-#include "gen.hpp"
 #include "matcher.hpp"
 #include "print.hpp"
 #include "io.hpp"
 #include "config.hpp"
 
+// backbone includes
+#include "pass.hpp"
+
+// tooling includes
+#include "select.hpp"
+#include "bbfmt.hpp"   // print_toplevel
+
 int main(int argc, char* argv[])
   {
-    if (argc != 2)
+    if (argc != 4)
       {
         // CONSIDER: reading from stdin
         // CONSIDER putting argv[0] as the exec name
@@ -31,15 +37,19 @@ int main(int argc, char* argv[])
       }
 
     const Texp selector = Parser::parseTexp(selector_str);
-
-    // read program
     const Texp program = parse_from_file(filename);
-
-    // println(program.tabs());
+    Texp curr = run_passes_until(program, passname);
 
     // print file
-    println("; CMD: bbfmt ", argv[1]);
+    println("; CMD: plumber ", argv[1], " ", argv[2], " '", argv[3], "'");
     println("; FILENAME: \"", program.value, "\"");
 
+    // print after passrunner?
+    // for (const auto& toplevel : curr)
+    //   {
+    //     println(toplevel.paren());
+    //     return 0;
+    //   }
 
+    select(curr, selector, [](const Texp& match){ print_toplevel(match); });
   }
