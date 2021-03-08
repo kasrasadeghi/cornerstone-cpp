1c15da2d3089c422792636a77d8afde34dd2a5be
kasrasadeghi <kasra.sadeghi1@gmail.com>
2021-03-08 18:25:35 -0500

    pass_config: extract get_passlist from backbone

--- backbone/pass_config.hpp
+++ backbone/pass_config.hpp
@@ -7,4 +7,6 @@ struct PassConfig {
   const std::vector<std::pair<std::string_view, std::function<void(Texp&)>>> pass_table;
 
   Texp run_all_passes(const Texp& tree);
+
+  std::string get_passlist(void);
 };
