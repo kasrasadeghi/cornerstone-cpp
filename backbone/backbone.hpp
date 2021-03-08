af4abf689a17fa4deea7a165048123be34a12918
kasrasadeghi <kasra.sadeghi1@gmail.com>
2021-03-08 18:21:14 -0500

    pass_config: extract run_all_passes from backbone

--- backbone/backbone.hpp
+++ backbone/backbone.hpp
@@ -6,8 +6,6 @@ namespace backbone {
   extern PassConfig pass_config;
 };
 
-Texp run_all_passes(const Texp& tree);
-
 std::string get_passlist(void);
 
 bool is_pass(std::string_view s);
--- backbone/pass_config.hpp
+++ backbone/pass_config.hpp
@@ -5,4 +5,6 @@
 
 struct PassConfig {
   const std::vector<std::pair<std::string_view, std::function<void(Texp&)>>> pass_table;
+
+  Texp run_all_passes(const Texp& tree);
 };
