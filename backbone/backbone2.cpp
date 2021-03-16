1a03c192dd74e512c0de587864824b874b65a732
kasrasadeghi <kasra.sadeghi1@gmail.com>
2021-03-16 17:35:04 -0400

    start work on bb2 grammars

--- backbone/backbone2.cpp
+++ backbone/backbone2.cpp
@@ -10,38 +10,38 @@
 PassConfig backbone2 {
   .pass_table = {
     {"include",   [](Texp& t) {
-      constexpr std::string_view filename = "bb-type-tall-str-include.texp";
+      constexpr std::string_view filename = "bb2-type-tall-str-include.texp";
 
       Grammar g {parse_from_file(std::string(GRAMMAR_DIR) + std::string(filename))[0]};
       Matcher m {g};
-      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb-type-tall-str-include Program:\n  " + t.paren());
+      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb2-type-tall-str-include Program:\n  " + t.paren());
       Includer i {g, m};
       t = i.Program(t, proof);
     }},
     {"str",       [](Texp& t) {
-      constexpr std::string_view filename = "bb-type-tall-str.texp";
+      constexpr std::string_view filename = "bb2-type-tall-str.texp";
 
       Grammar g {parse_from_file(std::string(GRAMMAR_DIR) + std::string(filename))[0]};
       Matcher m {g};
-      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb-type-tall-str Program:\n  " + t.paren());
+      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb2-type-tall-str Program:\n  " + t.paren());
       Str s {g, m};
       t = s.Program(t, proof);
     }},
     {"normalize", [](Texp& t) {
-      constexpr std::string_view filename = "bb-type-tall.texp";
+      constexpr std::string_view filename = "bb2-type-tall.texp";
 
       Grammar g {parse_from_file(std::string(GRAMMAR_DIR) + std::string(filename))[0]};
       Matcher m {g};
-      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb-type-tall Program:\n  " + t.paren());
+      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb2-type-tall Program:\n  " + t.paren());
       Normalize p {g, m};
       t = p.Program(t, proof);
     }},
     {"typeinfer", [](Texp& t) {
-      constexpr std::string_view filename = "bb-type.texp";
+      constexpr std::string_view filename = "bb2-type.texp";
 
       Grammar g {parse_from_file(std::string(GRAMMAR_DIR) + std::string(filename))[0]};
       Matcher m {g};
-      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb-type Program:\n  " + t.paren());
+      Texp proof = RESULT_UNWRAP(m.is(t, "Program"), "given texp is not a bb2-type Program:\n  " + t.paren());
       TypeInfer p {g, m};
       t = p.Program(t, proof);
     }},
